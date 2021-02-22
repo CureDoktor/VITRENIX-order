7c05585a6125892d9e1a845f095b1db07ca177dc
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2021-02-22 22:35:12 +0100

    22_02 Starting project

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function oneMonth() {
+  document.getElementById("oneMonth1").style.display = "block";
+  document.getElementById("oneMonth").style.display = "none";
+
+  document.getElementById("sixMonth1").style.display = "block";
+  document.getElementById("sixMonth").style.display = "none";
+
+  document.getElementById("threeMonth").style.display = "block";
+  document.getElementById("threeMonth1").style.display = "none";
+}
+
+function sixMonths() {
+  document.getElementById("oneMonth").style.display = "block";
+  document.getElementById("oneMonth1").style.display = "none";
+
+  document.getElementById("sixMonth").style.display = "block";
+  document.getElementById("sixMonth1").style.display = "none";
+
+  document.getElementById("threeMonth").style.display = "block";
+  document.getElementById("threeMonth1").style.display = "none";
+}
+
+function threeMonths() {
+  document.getElementById("oneMonth").style.display = "block";
+  document.getElementById("oneMonth1").style.display = "none";
+
+  document.getElementById("sixMonth1").style.display = "block";
+  document.getElementById("sixMonth").style.display = "none";
+
+  document.getElementById("threeMonth1").style.display = "block";
+  document.getElementById("threeMonth").style.display = "none";
+}

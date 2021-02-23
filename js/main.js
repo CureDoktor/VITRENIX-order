6978968da982dde6a6ac2723206b9a0f3a09bc9c
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2021-02-23 22:48:31 +0100

    23/02 Updates

--- js/main.js
+++ js/main.js
@@ -30,3 +30,29 @@ function threeMonths() {
   document.getElementById("threeMonth1").style.display = "block";
   document.getElementById("threeMonth").style.display = "none";
 }
+
+function creditCard() {
+  document.getElementById("ccNormal").style.display = "none";
+  document.getElementById("ccSelected").style.display = "block";
+
+  document.getElementById("paypalNormal").style.display = "block";
+  document.getElementById("paypalSelected").style.display = "none";
+}
+
+function paypal() {
+  document.getElementById("ccNormal").style.display = "block";
+  document.getElementById("ccSelected").style.display = "none";
+
+  document.getElementById("paypalNormal").style.display = "none";
+  document.getElementById("paypalSelected").style.display = "block";
+}
+
+function partOneContinue() {
+  document.getElementById("partOne").style.display = "none";
+  document.getElementById("partTwo").style.display = "block";
+}
+
+function partTwoContinue() {
+  document.getElementById("partTwo").style.display = "none";
+  document.getElementById("partThree").style.display = "block";
+}
